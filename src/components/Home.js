dcf94eda6339eef2ba78caecdfbfb0cb9b8d4fc0
Melina Lencina <melina00lencina@gmail.com>
2022-09-10 13:12:27 -0300

    :sparkles: Added navbar

---
+++ src/components/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+    return (
+        <div className="wrapped">
+            <div className="title">
+            </div>
+</div>
+    )
+}
+
+export default Home;
