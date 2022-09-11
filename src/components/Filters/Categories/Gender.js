8083c853aeafcc6c9185a693bb25f3fd13aee0f4
Franco Yunes <franco.yunes@hotmail.com>
2022-09-11 19:07:12 -0300

    :sparkles: added styling for chars

--- src/components/Filters/Categories/Gender.js
+++ src/components/Filters/Categories/Gender.js
@@ -6,7 +6,7 @@ const Gender = ({ setGender, setPageNumber }) => {
     return (
         <div className="accordion-item">
     <h2 className="accordion-header" id="headingOne">
-    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+    <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
         Gender
       </button>
     </h2>
--- src/components/Rickandmortyfetch.js
+++ src/components/Rickandmortyfetch.js
@@ -14,35 +14,36 @@ function Rickandmortyfetch() {
     let [gender, setGender] = useState("");
     let [species, setSpecies] = useState("");
     let [fetchedData, updateFetchedData] = useState([]);
-    let {info, results} = fetchedData;
-    
-    
+    let { info, results } = fetchedData;
+
+
     let URL_API = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}&status=${status}&gender=${gender}&species=${species}`;
 
-    useEffect(()=>{
-        (async function() {
-            let data = await fetch(URL_API).then(res=>res.json())
+    useEffect(() => {
+        (async function () {
+            let data = await fetch(URL_API).then(res => res.json())
             updateFetchedData(data);
-            
+
         })()
-    },[URL_API]);
+    }, [URL_API]);
 
     return (
-        <>
-        <Search setPageNumber={setPageNumber} setSearch={setSearch}/>          
-        <div className="container">
-            <div className="row">                
-                    <Filters setSpecies={setSpecies} setGender={setGender} setStatus={setStatus} setPageNumber={setPageNumber} />                
-                <div className="col-lg-8 col-12">
-                    <div className="row">
-                        <Characters results={results}/>
+
+        <div className="main-characters">
+            <Search setPageNumber={setPageNumber} setSearch={setSearch} />
+            <div className="container">
+                <div className="row">
+                    <Filters setSpecies={setSpecies} setGender={setGender} setStatus={setStatus} setPageNumber={setPageNumber} />
+                    <div className="col-lg-8 col-12">
+                        <div className="row">
+                            <Characters results={results} />
+                        </div>
                     </div>
-                </div>                
+                </div>
             </div>
+            <Pagination info={info} pageNumber={pageNumber} setPageNumber={setPageNumber} />
         </div>
-        <Pagination info={info} pageNumber={pageNumber} setPageNumber={setPageNumber} />
-        </>       
-        
+
     )
 
 }
