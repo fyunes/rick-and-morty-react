cc4652128be90c6627d591ec0b62a9d2062064d9
Melina Lencina <melina00lencina@gmail.com>
2022-09-11 04:25:14 -0300

    :sparkles: Added routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
+import React from 'react';
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Form from "./components/Form";
+import Rickandmortyfetch from "./components/Rickandmortyfetch";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <>
-    <div className="App">   
-    <Navbar />  
-    </div>
+      <div className="App">   
+        <Navbar />  
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/form" element={<Form />} />
+          <Route path="/rickandmorty" element={<Rickandmortyfetch />} />
+        </Routes>
+      </div>
     </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 import "./styles/App.css";
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
-    <App />
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
+    
 );
